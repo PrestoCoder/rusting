d7000c6080074a3a4a7eb2013f5cc075841550e5
PrestoCoder <f20160228g@alumni.bits-pilani.ac.in>
2026-03-25 01:49:06 -0500

    chore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
 #![allow(unused)]
 
-fn main() {}
+// would panic if y = 0
+fn div(x: i32, y: i32) -> i32 {
+    x / y
+}
+
+// Even better would be to return error as enums(fixed number)
+fn better_div(x: i32, y: i32) -> Result<i32, String> {
+    match y {
+        0 => Err("0 denominator mein kyun rakha bosdi".to_string()),
+        _ => Ok(x / y),
+    }
+}
+
+#[derive(Debug)]
+enum MathError {
+    DivisionByZero,
+    Other,
+}
+fn best_div(x: i32, y: i32) -> Result<i32, MathError> {
+    match y {
+        0 => Err(MathError::DivisionByZero),
+        _ => Ok(x / y),
+    }
+}
+
+fn main() {
+    // Error
+    // Simplest thing is to let the program crash - using panic!
+    // If panic in main, entire program terminates
+    // If in a thread, only that thread terminates
+    // panic!("chud gye guru");
+
+    // Option or Result
+    let arr = [1, 2, 3];
+    // Rust compiler won't even let this compile, let alone panic
+    // arr[9];
+
+    // Option
+    // Which is why there is a better function for this.
+    // P.S.: get function returns pointer, because it can be used to return a slice as well
+    // ex: arr.get(1..4)
+    let element = arr.get(0);
+    match element {
+        Some(value) => println!("value is {value}"),
+        None => println!("Oops! tie tie fish"),
+    };
+
+    // Result
+    div(2, 1);
+    match best_div(2, 0) {
+        Ok(val) => println!("result = {val}"),
+        Err(err) => println!("Error = {err:?}"),
+    }
+}
